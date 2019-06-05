4f0bfa6a1ea7df6735b40d6ece45353babf43a43
Eduardo Velez <evelez1990@hotmail.com>
2019-06-04 20:50:39 -0400

    Initial Commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Eduardo Velez on 6/4/19.
+//  Copyright © 2019 Eduardo Velez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
