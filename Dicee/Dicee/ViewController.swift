c98762ed3c52b3a7f0f94bbb181d18c1e6f96eef
Eduardo Velez <evelez1990@hotmail.com>
2019-06-09 21:04:53 -0400

    Initial Commit

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Eduardo Velez on 6/9/19.
+//  Copyright © 2019 Eduardo Velez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
