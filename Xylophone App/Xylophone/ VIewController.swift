24bdcf03954f897e0f59e9175184e4770ca2edbb
Eduardo Velez <evelez1990@hotmail.com>
2019-07-07 21:51:28 -0400

    Note array have been added

--- Xylophone App/Xylophone/ VIewController.swift
+++ Xylophone App/Xylophone/ VIewController.swift
@@ -5,12 +5,16 @@
 //  Created by Angela Yu on 27/01/2016.
 //  Copyright © 2016 London App Brewery. All rights reserved.
 //
+// Student: Eduardo Velez
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
-
+    var audioPlayer : AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +23,22 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        var selectedSoundFileName : String = soundArray[sender.tag - 1]
         
+        playSound()
+        
+    }
+    
+    func playSound() {
+        
+        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
         
     }
     
