92fc99c2e60ba1a36b3429edc2a634533aa85da2
Eduardo Velez <evelez1990@hotmail.com>
2019-08-17 18:50:37 -0400

    Clima Initial Commit

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Eduardo Velez on 8/4/19.
+//  Copyright © 2019 Eduardo Velez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
