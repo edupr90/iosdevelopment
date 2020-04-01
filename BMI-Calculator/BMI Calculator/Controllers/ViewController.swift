b9e74727533e2750afd4029b4ae052df9c0e3158
Eduardo Velez <evelez1990@hotmail.com>
2020-04-01 00:01:05 -0400

    Added Weight and height values

--- BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -10,18 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
         
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)kg"
     }
     
 }
